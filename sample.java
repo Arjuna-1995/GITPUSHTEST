7fd82b85dd1bd6a3654235b88b08393b02493fe3
Arjuna-1995 <aarjuna21@gmail.com>
2024-02-06 20:39:34 +0530

    git

--- sample.java
+++ sample.java
@@ -1,11 +1,13 @@
 public class sample {
     public static void main(String[] args) {
         {
-            int c,a=10,b=5;
+            int c,a=10,b=5,d,e;
             c=a-b;
             d=a-b;
+            e=a*b;
             System.out.println(d);
             System.out.println(c);
+            System.out.println(e);
         }
     }
     
