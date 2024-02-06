c8f6b07ecaaef6c6cccab825b177814ef9f51a94
Arjuna-1995 <aarjuna21@gmail.com>
2024-02-06 09:46:35 +0530

    add one file

---
+++ sample.java
@@ -0,0 +1,10 @@
+public class sample {
+    public static void main(String[] args) {
+        {
+            int c,a=10,b=5;
+            c=a-b;
+            System.out.println(c);
+        }
+    }
+    
+}
